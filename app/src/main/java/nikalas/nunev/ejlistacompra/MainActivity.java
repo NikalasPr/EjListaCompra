32961c3ab163018f8d6fa3baa9a81d842ec0840c
finni <nikalas.boyanov.nunev@cieep.com>
2023-10-31 12:41:37 +0100

    Initial commit

---
+++ app/src/main/java/nikalas/nunev/ejlistacompra/MainActivity.java
@@ -0,0 +1,31 @@
+package nikalas.nunev.ejlistacompra;
+
+import android.os.Bundle;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.view.View;
+
+
+import nikalas.nunev.ejlistacompra.databinding.ActivityMainBinding;
+
+
+public class MainActivity extends AppCompatActivity {
+
+    private ActivityMainBinding binding;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+
+        binding = ActivityMainBinding.inflate(getLayoutInflater());
+        setContentView(binding.getRoot());
+
+        binding.fab.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+
+            }
+        });
+    }
+}
